ae6ac5cbd50c32cc07769ab845c1eb20d8b6c37b
Fabrice Nemo <fabnem12@gmail.com>
2023-08-18 09:22:49 +0200

    word train

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import asyncio
 import nextcord as discord
 from nextcord.ext import commands
+from unidecode import unidecode
 
 import constantes
+
+wordTrainChannel = 1141992409165733988
 
 async def dmChannelUser(user):
     if user.dm_channel is None:
@@ -16,10 +19,41 @@ def main():
     @bot.event
     async def on_message(message):
         await bot.process_commands(message)
+        await verif_word_train(message)
+
+    async def verif_word_train(message):
+        """
+        Word train channel:
+        your word has to start with the same letter as the word before ends. Example "The elephant tries snorkeling..."
+        """
+        
+        if message.channel.id != wordTrainChannel:
+            return
+    
+        isLetter = lambda x: x in "abcdefghijklmnopqrstuvwxyz "
+    
+        msgTxt = unidecode(message.content.lower())
+        msgLetters = "".join(filter(isLetter, msgTxt))
+        words = msgLetters.split()
+
+        if len(words) > 1:
+            lastLetter = words[0][-1]
+
+            for word in words[1:]:
+                if word[0] != lastLetter:
+                    await message.delete()
+                    await message.channel.send(f"<:bonk:843489770918903819> {message.author.mention}")
+
+                    return False
+                else:
+                    lastLetter = word[-1]
+
+        return True
+            
 
     @bot.command(name="màj")
     async def maj(ctx):
-        if ctx.author.id == 619574125622722560:
+        if ctx.author.id == constantes.mainAdminId:
             from subprocess import Popen, DEVNULL
 
             await ctx.message.add_reaction("👌")
